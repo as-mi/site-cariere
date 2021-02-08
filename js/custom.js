d905075f1839abbff814f996a08c4c2194c9bb81
Buhai Darius <buhaidarius@gmail.com>
2021-02-08 20:57:56 +0200

    Terminal update

--- js/custom.js
+++ js/custom.js
@@ -3,36 +3,47 @@ $('.js-tilt').tilt({
 })
 
 $(function() {
-    $('#input').val("help");
     $('.terminal').on('click', function(){
         $('#input').focus();
     });
     $('#input').on('keydown',function search(e) {
         if(e.keyCode === 13) {
-            let input = $(this).val();
-            $('#history').append($('#path')[0].innerHTML+input+'<br>');
-            let out = manageTerminalOutput(input);
-            if(out!==false)
-                $('#history').append(out+'<br>');
-            $('#input').val('');
+            execCommand($(this).val())
         }
     });
+    setTimeout(function(){
+        execCommand("Cariere.exe");
+        $('#input').val("help");
+    }, 100);
 });
 
+function execCommand(input){
+    $('#history').append($('#path')[0].innerHTML+input+'<br>');
+    let out = manageTerminalOutput(input);
+    if(out!==false)
+        $('#history').append(out+'<br>');
+    $('#input').val('');
+}
+
 function manageTerminalOutput(input){
+    let input_low = input.toLocaleString().toLowerCase();
     if(input.substring(0, 3)==='cd '){
         $('#path').html(input.substring(3) + '>&nbsp;');
         return false;
     }
-    if(input==="Cariere.exe")
-        return "CARIERE v10 Loading... [#############___________] 50%";
-    if(input==="h" || input==="help")
-        return "ls - list files<br>cd - change directory<br>clear - clear terminal";
-    if(input==="clear"){
+    if(input==="Cariere.exe"){
+        return "CARIERE v10 Loading... [##########__________] 50%";
+    }
+    if(input_low==="h" || input_low==="help")
+        return "ls -> list files" +
+            "<br>cd -> change directory" +
+            "<br>clear -> clear terminal" +
+            "<br>help -> list commands";
+    if(input_low==="clear"){
         $('#history')[0].innerHTML = "";
         return false;
     }
-    if(input==="ls" || input==="l")
+    if(input_low==="ls" || input_low==="l")
         return "Cariere.exe";
     return "command not found: "+input;
 }
